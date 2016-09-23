cbe64b5e8f55ccf16217e3f6ae04a381c659b3ca
Caoli <39412442@qq.com>
2016-09-23 11:23:52 +0800

    modify server.c

--- server/server.c
+++ server/server.c
@@ -2,13 +2,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 	int    listenfd, connfd;
 	pid_t  childpid;
 	socklen_t chilen;
 	struct sockaddr_in cliaddr, servaddr;
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;/*
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
