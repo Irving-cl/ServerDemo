7b11f96c189df50d8d674f7f4f4b97ed7a663d89
Irving-cl <oos1111@sjtu.edu.cn>
2016-09-28 12:13:03 +0800

    reformat

--- server/server.c
+++ server/server.c
@@ -7,17 +7,17 @@
 #include <unistd.h>
 #include <string.h>
 
-#define	SA	struct sockaddr
+#define SA  struct sockaddr
 
-#define	LISTENQ        1024     /* 等待连接队列的最大长度 */
-#define	MAXLINE		   4096	    /* 一行的最大长度 */
+#define LISTENQ        1024     /* 等待连接队列的最大长度 */
+#define MAXLINE        4096     /* 一行的最大长度 */
 #define SERV_PORT      9877
 
 /* 
  * in_addr结构
  * typedef uint32_t in_addr_t;
  * struct in_addr {
- * 	   in_addr_t s_addr;
+ *     in_addr_t s_addr;
  * }
  */
 
@@ -32,47 +32,47 @@
  */
 
 void str_echo(int sockfd) {
-	ssize_t    n;
-	char       buf[MAXLINE];
+    ssize_t    n;
+    char       buf[MAXLINE];
 
 again:
-	while ((n = read(sockfd, buf, MAXLINE)) > 0) {
-		write(sockfd, buf, n);
-	}
-	if (n < 0 && errno == EINTR) {    // EINTR：信号中断造成的错误返回值
-		goto again;
-	} else if (n < 0) {
-		perror("str_echo: read error");
-		exit(1);
-	}
+    while ((n = read(sockfd, buf, MAXLINE)) > 0) {
+        write(sockfd, buf, n);
+    }
+    if (n < 0 && errno == EINTR) {    // EINTR：信号中断造成的错误返回值
+        goto again;
+    } else if (n < 0) {
+        perror("str_echo: read error");
+        exit(1);
+    }
 }
 
 int main(int argc, char **argv) {
-	int    listenfd, connfd;
-	pid_t  childpid;
-	socklen_t clilen;    // 即int，客户端地址长度
-	struct sockaddr_in cliaddr, servaddr;
+    int    listenfd, connfd;
+    pid_t  childpid;
+    socklen_t clilen;    // 即int，客户端地址长度
+    struct sockaddr_in cliaddr, servaddr;
 
-	/*
-	 * 创建TCP套接字，并且转换成一个监听套接字 
-	 */
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);            // 根据指定的地址族，数据类型和协议创建一个套接字
-	bzero(&servaddr, sizeof(servaddr));                    // servaddr清零
-	servaddr.sin_family = AF_INET;                         // 设置地址族
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);          // host to network long，将一个32位数从主机字节顺序转换成网络字节顺序
-	servaddr.sin_port = htons(SERV_PORT);                  // host to network short，将一个16位数从主机字节顺序转换成网络字节顺序
-	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));    // 绑定socket和addr
-	listen(listenfd, LISTENQ);                             // 使套接字开始监听，第二个参数backlog是等待连接队列的最大长度
+    /*
+     * 创建TCP套接字，并且转换成一个监听套接字 
+     */
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);            // 根据指定的地址族，数据类型和协议创建一个套接字
+    bzero(&servaddr, sizeof(servaddr));                    // servaddr清零
+    servaddr.sin_family = AF_INET;                         // 设置地址族
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);          // host to network long，将一个32位数从主机字节顺序转换成网络字节顺序
+    servaddr.sin_port = htons(SERV_PORT);                  // host to network short，将一个16位数从主机字节顺序转换成网络字节顺序
+    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));    // 绑定socket和addr
+    listen(listenfd, LISTENQ);                             // 使套接字开始监听，第二个参数backlog是等待连接队列的最大长度
 
 
-	for ( ; ; ) {
-		clilen = sizeof(cliaddr);
-		connfd = accept(listenfd, (SA *) &cliaddr, &clilen);    // 阻塞于accept调用，等待客户连接完成
-		if ((childpid = fork()) == 0) {                    // 为每一个客户派生一个处理它们的子进程
-			close(listenfd);                               // 关闭监听描述符
-			str_echo(connfd);                              // 回射
-			exit(0);                                       // 子进程任务完成，终止
-		}
-		close(connfd);                                     // 关闭父进程的已连接描述符(引用计数减1)，防止这个套接字永远也关不掉
-	}
+    for ( ; ; ) {
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (SA *) &cliaddr, &clilen);    // 阻塞于accept调用，等待客户连接完成
+        if ((childpid = fork()) == 0) {                    // 为每一个客户派生一个处理它们的子进程
+            close(listenfd);                               // 关闭监听描述符
+            str_echo(connfd);                              // 回射
+            exit(0);                                       // 子进程任务完成，终止
+        }
+        close(connfd);                                     // 关闭父进程的已连接描述符(引用计数减1)，防止这个套接字永远也关不掉
+    }
 }
