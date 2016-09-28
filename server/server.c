37a9185d2e95ed9ded0768cb384aa54591b3ac0f
Irving-cl <oos1111@sjtu.edu.cn>
2016-09-28 11:35:51 +0800

    modify server.c

--- server/server.c
+++ server/server.c
@@ -4,9 +4,11 @@
 #include <unistd.h>
 #include <string.h>
 
-#define	LISTENQ        1024
+#define	SA	struct sockaddr
+
+#define	LISTENQ        1024     /* 等待连接队列的最大长度 */
+#define	MAXLINE		   4096	    /* 一行的最大长度 */
 #define SERV_PORT      9877
-#define INADDR_ANY     ((unsigned long int) 0x00000000)
 
 /* 
  * in_addr结构
@@ -26,10 +28,25 @@
  * }
  */
 
+void str_echo(int sockfd) {
+	ssize_t    n;
+	char       buf[MAXLINE];
+
+again:
+	while ((n = read(sockfd, buf, MAXLINE)) > 0) {
+		writen(sockfd, buf, n);
+	}
+	if (n < 0 && errno == EINTR) {    // EINTR：信号中断造成的错误返回值
+		goto again;
+	} else if (n < 0) {
+		err_sys("str_echo: read error");
+	}
+}
+
 int main(int argc, char **argv) {
 	int    listenfd, connfd;
 	pid_t  childpid;
-	socklen_t chilen;
+	socklen_t chilen;    // 即int，客户端地址长度
 	struct sockaddr_in cliaddr, servaddr;
 
 	/*
@@ -41,18 +58,17 @@ int main(int argc, char **argv) {
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);          // host to network long，将一个32位数从主机字节顺序转换成网络字节顺序
 	servaddr.sin_port = htons(SERV_PORT);                  // host to network short，将一个16位数从主机字节顺序转换成网络字节顺序
 	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));    // 绑定socket和addr
-	Listen(listenfd, LISTENQ);                             // 使套接字开始监听，第二个参数backlog是等待连接队列的最大长度
+	listen(listenfd, LISTENQ);                             // 使套接字开始监听，第二个参数backlog是等待连接队列的最大长度
 
 
-	/*
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
-		if ( (childpid = Fork()) == 0) {
-			Close(listenfd);
-			str_echo(connfd);
-			exit(0);
+		connfd = accept(listenfd, (SA *) &cliaddr, &clilen);    // 阻塞于accept调用，等待客户连接完成
+		if ((childpid = fork()) == 0) {                    // 为每一个客户派生一个处理它们的子进程
+			close(listenfd);                               // 关闭监听描述符
+			str_echo(connfd);                              // 回射
+			exit(0);                                       // 子进程任务完成，终止
 		}
-		Close(connfd);
-	}*/
+		close(connfd);                                     // 关闭父进程的已连接描述符(引用计数减1)，防止这个套接字永远也关不掉
+	}
 }
