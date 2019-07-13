82f971666a4cdfdc797854f25c7646be32c303e8
Li Cao <irvingcl@google.com>
2019-07-13 20:09:13 +0800

    [build] add tests

--- tests/test1.py
+++ tests/test1.py
@@ -4,7 +4,7 @@ import unittest
 class TestForFun(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(1 + 1 == 2)
+        self.assertEqual(1 + 1, 2)
 
 if __name__ == '__main__':
     unittest.main()
